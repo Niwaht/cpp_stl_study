2d8166812bad8c161d5e51c0d68bbe8de1892b39
Cygnus <niwaht.u@gmail.com>
2025-05-17 23:13:00 +0700

    Create C_STL_CppNut_17-Pair.cpp

---
+++ C_STL_CppNut_17-Pair.cpp
@@ -0,0 +1,42 @@
+// TOPIC: std::pair in C++
+
+// SYNTAX: std::pair<type1, type2> obj;
+
+// NOTES:
+// 0. std::pair is a struct template that provides a way to store two heterogeneous objects as a single unit.
+// 1. map, multimap, unorder_map, unorder_multimap can use pair to insert data into their structures.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(std::pair<int,int>& obj) { cout << obj.first << " " << obj.second << endl; }
+
+int main()
+{
+    {
+        std::pair<int, int> obj(10, 20);
+        print(obj);
+    }
+
+    {
+        std::pair<int, int> obj = std::make_pair(10, 20);
+        print(obj);
+    }
+
+    {
+        std::vector<std::pair<std::string, int>> list;
+        list.push_back(make_pair("Rupesh", 30));
+        list.push_back(make_pair("Hitesh", 20));
+        list.push_back(std::pair<std::string, int>("Bhupendra", 22));
+        list.push_back(std::pair<std::string, int>("Sagar", 18));
+
+        for (auto& elm: list) {
+            cout << elm.first << " " << elm.second << endl;
+        }
+
+    }
+
+    return 0;
+}
