19d5a28a8ebb75e74e2f1760ff25aa64c5715fff
Cygnus <niwaht.u@gmail.com>
2026-01-16 23:58:00 +0700

    Update CPC_15_ShortestPath.cpp

--- CPC_15_ShortestPath.cpp
+++ CPC_15_ShortestPath.cpp
@@ -135,11 +135,126 @@ void main_3() {
     long long res = spfa(s, t, n, adj);
     if(res == -2) cout << "Negative cycle detected\n";
     else if(res == INF) cout << "Node unreachable\n";
-    else cout << res << '\n';
+    else cout << "Shortest path's length: " << res << '\n';
+}
+
+/// Small label first (SLF) & Large label last (LLL)
+#include <deque>
+#include <limits>
+void spfa_optimized(int n, int start_node, const vector<vector<edge>>& adj) {
+    vector<long long> dist(n + 1, INF);
+    vector<bool> in_queue(n + 1, false);
+    deque<int> dq;
+    long long sum_dist = 0;
+    dist[start_node] = 0; dq.push_back(start_node); in_queue[start_node] = true; sum_dist += dist[start_node];
+    while(!dq.empty()) {
+        // LLL optimization
+        while(dist[dq.front()] * (long long)dq.size() > sum_dist) {
+            int move_node = dq.front(); dq.pop_front(); dq.push_back(move_node);
+        }
+        int u = dq.front(); dq.pop_front(); in_queue[u] = false; sum_dist -= dist[u];
+        for(const auto& edge : adj[u]) {
+            int v = edge.to, w = edge.weight;
+            if(dist[u] + w < dist[v]) {
+                if(in_queue[v]) sum_dist -= dist[v];
+                dist[v] = dist[u] + w;
+                sum_dist += dist[v];
+                if(!in_queue[v]) {
+                    // SLF optimization
+                    (!dq.empty() && dist[v] < dist[dq.front()]) ? dq.push_front(v) : dq.push_back(v);
+                    in_queue[v] = true;
+                }
+            }
+        }
+    }
+    cout << "SUMMARY:\n";
+    for(int i = 1; i <= n; i++) {
+        if(i == start_node) continue;
+        cout << "From node " << start_node << " to " << i << ", ";
+        if(dist[i] == INF) cout << "node is unreachable\n";
+        else cout << "shortest path's length: " << dist[i] << '\n';
+    }
+}
+void main_4() {
+    cout << "\nSLF & LLL\n----------------------------------------\n";
+    /*
+        int n, m, start_node;
+        cin >> n >> m;
+        vector<vector<edge>> adj(n + 1);
+        for(int i = 0; i < m; i++) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            adj[u].push_back({v, w});
+        }
+        cin >> start_node;
+        spfa_optimized(n, start_node, adj);
+    */
+
+    int n = 4, start_node = 1;
+    vector<vector<edge>> adj(n + 1);
+    auto push = [&](int u, int v, int w){ adj[u].push_back({v, w}); };
+    push(1, 2, 100); push(1, 3, 20); push(3, 2, 2); push(2, 4, 5);
+    spfa_optimized(n, start_node, adj);
+}
+
+/// Floyd-Warshall algorithm
+#include <string>
+#define INF INF_5
+const int INF = 1e9;
+void floydWarshall(int n, vector<vector<int>>& dist) {
+    for(int k = 1; k <= n; k++) {
+        for(int i = 1; i <= n; i++) {
+            if(dist[i][k] == INF) continue;
+            for(int j = 1; j <= n; j++)
+                if(dist[k][j] < INF) { if(dist[i][k] + dist[k][j] < dist[i][j]) dist[i][j] = dist[i][k] + dist[k][j]; }
+        }
+    }
+}
+bool hasNegativeCycle(int n, const vector<vector<int>>& dist) {
+    for(int i = 1; i <= n; i++) { if(dist[i][i] < 0) return true; }
+    return false;
+}
+void main_5() {
+    cout << "\nFloyd-Warshall algorithm\n----------------------------------------\n";
+    /*
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> dist(n + 1, vector<int>(n + 1, INF));
+        for(int i = 1; i <= n; i++) dist[i][i] = 0;
+        while(m--) {
+            int u, v, w; cin >> u >> v >> w;
+            if(w < dist[u][v]) dist[u][v] = w;
+        }
+        floydWarshall(n, dist);
+        cout << "SUMMARY:\nUN = Node unreacable\n";
+        if(hasNegativeCycle(n, dist)) cout << "Negative cycle detected\n";
+        else {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) { (dist[i][j] == INF) ? cout << "UN " : cout << dist[i][j] << ' '; }
+                cout << '\n';
+            }
+        }
+    */
+
+    int n = 7;
+    vector<vector<int>> dist(n + 1, vector<int>(n + 1, INF));
+    for(int i = 1; i <= n; i++) dist[i][i] = 0;
+    auto push = [&](int u, int v, int w){ if(w < dist[u][v]) dist[u][v] = w; };
+    push(1, 2, 30); push(1, 3, 9); push(2, 6, -20); push(3, 4, 6);
+    push(3, 5, 2); push(4, 5, -10); push(4, 6, 9); push(6, 7, 1);
+    floydWarshall(n, dist);
+    cout << "SUMMARY:\nUN = Node unreacable\n";
+    if(hasNegativeCycle(n, dist)) cout << "Negative cycle detected\n";
+    else {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) { (dist[i][j] == INF) ? cout << "UN " : cout << dist[i][j] << ' '; }
+            cout << '\n';
+        }
+    }
 }
 
 int main()
 {
-    main_1(); main_2(); main_3();
+    main_1(); main_2(); main_3(); main_4(); main_5();
     return 0;
 }
